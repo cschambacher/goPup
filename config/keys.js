2b33f769493bbfb5a14f9b572e1f539e11463561
deb <lee025@gmail.com>
2020-01-24 19:44:03 -0500

    Deb made a whooopsie

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  MONGO_URI:
-    "mongodb+srv://dev:fSQJ0xdmi1YDJ1Up@cluster0-qtqda.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrPrivateKey: "d49e0d77-3625-41e4-9252-b6d1b0879565"
-};
