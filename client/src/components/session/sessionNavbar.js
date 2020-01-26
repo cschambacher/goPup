e67694a0a2b5c8f0739d5891e2f7b416fcca932d
deb <lee025@gmail.com>
2020-01-26 12:13:47 -0500

    lah dee dahh

--- client/src/components/session/sessionNavbar.js
+++ client/src/components/session/sessionNavbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { withRouter, Link } from "react-router-dom";
 import { Query, ApolloConsumer } from "react-apollo";
 import { IS_LOGGED_IN } from "../graphql/queries";
