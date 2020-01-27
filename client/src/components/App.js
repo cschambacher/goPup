056a947a1a1d8c36d415c39ec3d8cd0dd7b3d95c
deb <lee025@gmail.com>
2020-01-27 13:39:49 -0500

    Map Nav Skeleton

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <div>
       <Switch>
-        <Route path="/routes/new" component={MapAPI} />
+        <AuthRoute path="/routes/new" component={MapAPI} />
         <Route path="/(register|login)" component={SessionMaster} />
         <AuthRoute path="/" component={LandingMaster} />
         {/* <Route path="/" component={() => <div>Hello, there. Landing page belongs here!</div>} /> */}
